ca61293319b10258075af18dde3a435729963839
sadikcoban <coban.sadik@icloud.com>
2023-02-20 19:04:20 +0300

    implement delegate methods and game logic along with ui components

--- CowsAndBullsGame-MacOS/ViewController.swift
+++ CowsAndBullsGame-MacOS/ViewController.swift
@@ -7,30 +7,77 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+final class ViewController: NSViewController {
+    
     @IBOutlet weak var tableView: NSTableView!
     
     @IBOutlet weak var guess: NSTextField!
     
     
+    var answer = ""
+    var guesses = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startNewGame()
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
+    
     
     @IBAction func submitGuess(_ sender: Any) {
         
     }
-
+    
+    private func result(for guess: String) -> String {
+        "Result"
+    }
+    
+    private func startNewGame() {
+        guess?.stringValue = ""
+        guesses.removeAll()
+        answer = ""
+        var numbers = Array(0...9)
+        numbers.shuffle()
+        for _ in 0 ..< 4 {
+            answer.append(String(numbers.removeLast()))
+        }
+        tableView.reloadData()
+    }
 }
 
+
+
+// MARK: - Extensions
+
+extension ViewController: NSTableViewDataSource{
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        guesses.count
+    }
+    
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        if tableColumn?.title == "Guess" {
+            // this is the Guess column, show a previous guess
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            // this is the Result column, call result
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        return vw
+    }
+    
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        false
+    }
+}
